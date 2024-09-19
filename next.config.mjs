6adb7e8bb9a2011dbab100b1f475db7878c60d2a
Sushil Sharma <sushilsharma@Sushils-Mac-mini.local>
2024-09-19 18:35:03 +0530

    config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-        {
-          protocol: "http",
-          hostname: "**", 
-        },
-      ],
-    },
-    experimental: {
-      forceSwcTransforms: true,
-    },
-    server: {
-      https: true,
-    },
-  };
+const nextConfig = {};
 
 export default nextConfig;
