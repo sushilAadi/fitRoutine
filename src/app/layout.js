3c80aac3ccb26f25f3f1da44d8cf834e3cbfcfa5
Sushil Sharma <sushilsharma@Sushils-Mac-mini.local>
2024-09-22 14:07:52 +0530

    speedinsight added

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from "@material-tailwind/react";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 const queryClient = new QueryClient();
@@ -52,6 +53,7 @@ export default function RootLayout({ children }) {
       </GlobalContextProvider>
       </ThemeProvider>
       <Analytics />
+      <SpeedInsights />
       </body>
     </html>
     </QueryClientProvider>
