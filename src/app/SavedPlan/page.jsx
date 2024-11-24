0ef00d7c5109b7d902b277d3b9156fdf5384ec5c
Sushil Sharma <sushilsharma@Sushils-Mac-mini.local>
2024-11-24 20:58:57 +0530

    bug fixed

--- src/app/SavedPlan/page.jsx
+++ src/app/SavedPlan/page.jsx
@@ -28,7 +28,7 @@ const CustomPlanPage = () => {
   };
 
   return (
-    <div className="container h-screen px-4 py-2 mx-auto">
+    <div className="container h-screen px-4 py-2 mx-auto overflow-y-auto">
       <h1 className="mb-4 text-2xl font-bold">Saved Plans</h1>
       {savedPlans.length > 0 ? (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
